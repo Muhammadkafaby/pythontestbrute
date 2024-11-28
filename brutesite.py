9c517c9e15d1589afe165e1d363acfa4c4d66a1d
Muhammad Kafaby <114895955+Muhammadkafaby@users.noreply.github.com>
2024-11-28 16:20:12 +0000

    add time

--- brutesite.py
+++ brutesite.py
@@ -1,4 +1,5 @@
 import threading
+import time
 import requests
 
 url = "https://camo.githubusercontent.com/add76353a9c954e6b6cc0e1d79d0825d08b96b85ef6e4728dc5b882029274d42/68747470733a2f2f76697369746f722d62616467652d64656e6f2e64656e6f2e6465762f4d7568616d6d61646b61666162792e4d7568616d6d61646b61666162792e737667"
@@ -11,6 +12,7 @@ def send_requests():
         response = requests.get(url)
         print(f"Request {count + 1}: Status Code {response.status_code}")
         count += 1
+        time.sleep(1)  # Delay of 1 second between requests
 
 threads = []
 
